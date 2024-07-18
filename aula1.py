5e0c0a597c43d87fdd835c35b13cc79b1e7d63e0
JVitor21 <jvlimasantos0@gmail.com>
2024-07-18 16:43:59 -0300

    commit aula 1

---
+++ aula1.py
@@ -0,0 +1,14 @@
+"""
+DocString -> podem ter multiplas linhas
+E escrever o que eu quiser
+O interpretador lê isso
+"""
+'''
+Usar para escrever notas
+'''
+
+
+# Permite escrever um comentário -> linha unica
+print(123) # Na frente
+# Abaixo
+print(456)   
