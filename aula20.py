4e571909789a55b9e6bfb5b4f5f0f7f2b3051b6d
JVitor21 <jvlimasantos0@gmail.com>
2024-08-21 14:57:06 -0300

    Resposta do professor e minha

--- aula20.py
+++ aula20.py
@@ -1,13 +1,20 @@
 primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
+# Minha resposta
+#if primeiro_valor > segundo_valor:
+#    print('O primeiro valor', primeiro_valor, 'é maior que', 'o segundo_valor ', segundo_valor)
+#elif segundo_valor > primeiro_valor:
+#    print('O segundo valor', segundo_valor, 'é maior que ', 'o primeiro valor', primeiro_valor)
+#elif primeiro_valor == segundo_valor or segundo_valor == primeiro_valor:
+#    print('Os valores são iguais!')
+#else:
+#    print('Os valores são diferentes!')
 
-if primeiro_valor > segundo_valor:
-    print('O primeiro valor', primeiro_valor, 'é maior que', 'o segundo_valor ', segundo_valor)
-elif segundo_valor > primeiro_valor:
-    print('O segundo valor', segundo_valor, 'é maior que ', 'o primeiro valor', primeiro_valor)
-elif primeiro_valor == segundo_valor or segundo_valor == primeiro_valor:
-    print('Os valores são iguais!')
+# Resposta do professor
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=} é maior '
+          f'do que{segundo_valor=}')
 else:
-    print('Os valores são diferentes!')
-    
+    print(f'{segundo_valor=} é maior '
+          f'do que {primeiro_valor=}')
