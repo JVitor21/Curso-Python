5558b19c8fc6bf8c3e94f4c32d3fb3ae8ef08a2c
JVitor21 <jvlimasantos0@gmail.com>
2024-10-17 15:49:13 -0300

    Commit aula 36

---
+++ aula36.py
@@ -0,0 +1,29 @@
+"""
+Operadores de atribuição
+= . += . -= . /= . //= . **= . %=
+"""
+contador = 10
+# contador = '20'
+
+
+# while contador <= 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou')
+
+# contador = 2 # Igual
+# contador += 2 # Soma ou concatenação
+# contador -= 2 # Subtração
+# contador *= 2 # Multiplicação
+# contador /= 2 # Divisão
+# contador //= 2 # Divisão inteira
+# contador **= 2 # Potenciação
+# contador %= 2 # Modulo
+
+# contador += '2'
+
+# contador *= '2'
+
+
+print(contador)
