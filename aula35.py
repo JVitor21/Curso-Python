cab9ed01c80405e01a8d2827fc9b342edbceda07
JVitor21 <jvlimasantos0@gmail.com>
2024-10-17 15:48:28 -0300

    Commit aula 35

---
+++ aula35.py
@@ -0,0 +1,14 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+print('Contagem: ')
+while contador <= 10:
+    print(contador)
+
+    contador = contador + 1 
+    
+print('Acabou')
